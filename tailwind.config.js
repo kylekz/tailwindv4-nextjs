424bddff74e8bf602102086006cb1a19a94a3cc6
kylekz <kairu@team-reflex.com>
2024-11-23 17:36:39 +1300

    intellisense repro

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1250px",
+      },
+    },
+  },
+};
